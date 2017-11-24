845360239a8e5a742189ef68ab37ad52d59d7d89
bianchini <lore.bianchini@gmail.com>
2017-11-24 16:40:07 +0100

    fixes to marge bins

--- python/mix_data.py
+++ python/mix_data.py
@@ -19,51 +19,85 @@ class MixData:
         self.output_dir = output_dir
         return
 
-    def mix(self, mass=80.000, pdf=(lambda x : 1.0), params={}):
+    def mix(self, mass=80.000, pt_bin=[], y_bin=[], coeff=[], pdf=(lambda x : 1.0), params={}):
 
         pt_edges = np.array([])
         y_edges = np.array([])        
         mix = np.array([])
         for ipt,pt in enumerate(params['params_W']['pt']):
-            #for iy,y in enumerate(params['params_W']['y']):
+            if pt<pt_bin[0] or pt>=pt_bin[1]:
+                continue
+            print "pt %s is in range" % pt 
             for ic0,c0 in enumerate(params['params_W']['A0']):
+                if c0!=coeff[0]:
+                    continue
                 for ic1,c1 in enumerate(params['params_W']['A1']):
+                    if c1!=coeff[1]:
+                        continue
                     for ic2,c2 in enumerate(params['params_W']['A2']):
+                        if c2!=coeff[2]:
+                            continue
                         for ic3,c3 in enumerate(params['params_W']['A3']):
+                            if c3!=coeff[3]:
+                                continue
                             for ic4,c4 in enumerate(params['params_W']['A4']):
-                                out_name = 'pt{:02.1f}'.format(pt)+'_'+'y{:02.1f}'.format(0.00)+'_M{:05.3f}'.format(mass)
+                                if c4!=coeff[4]:
+                                    continue
+
+                                in_name = 'pt{:02.1f}'.format(pt)+'_'+'y{:02.1f}'.format(0.00)+'_M{:05.3f}'.format(mass)
+
                                 coeff = [c0,c1,c2,c3,c4]
                                 for c in range(5):
-                                    out_name += ('_A'+str(c)+('{:03.2f}'.format(coeff[c])))
-                                sample = np.load(self.input_dir+'/grid_lab_'+out_name+'.npy')
-                                #print sample
+                                    in_name += ('_A'+str(c)+('{:03.2f}'.format(coeff[c])))
 
-                                sam = copy.deepcopy(sample[0])
-                                for iy,y in enumerate(sample[1]):                                    
+                                # load the file
+                                sample = np.load(self.input_dir+'/grid_lab_'+in_name+'.npy')
+
+                                y_edges = sample[1]
+                                pt_edges = sample[2]
+
+                                # must be odd!
+                                #middle_point = (len(params['params_W']['y'])-1)/2
+                                middle_point = (len( y_edges )-1)/2
+                                y_bin_width = (y_edges[1]-y_edges[0])
+
+                                shift_pos = range( int((y_bin[0]-y_edges[0])/y_bin_width), 
+                                                   int((y_bin[1]-y_edges[0])/y_bin_width) + 1)
+                                        
+                                #for iy in [  middle_point-80,middle_point, middle_point+130]:
+                                #for iy,y in np.ndenumerate( y_edges ):            
+
+                                for iy in shift_pos:
+                                #for iy,y in np.ndenumerate(params['params_W']['y']):            
+                                    y = y_edges[0]+y_bin_width*iy
+                                    print "y %s is in range" % y 
+
                                     weight = pdf(pt,y, c0, c1, c2, c3, c4)
-                                    #print weight
+                                    #weight=1.0
 
-                                    if y>=0.:
-                                        sam[:,-1] = 0.
-                                        print iy-len(sample[1])/2 
-                                        np.roll(sam, iy-len(sample[1])/2  )
-                                    else:
-                                        sam[:,0] = 0.
-                                        np.roll(sam, iy+len(sample[1])/2 )                                        
+                                    shift = iy-middle_point
+                                    #shift = int(y/y_bin_width)
 
+                                    sam = copy.deepcopy(sample[0])
+                                    if shift>0:
+                                        sam[-shift:, :] = 0.
+                                    elif shift<0:
+                                        sam[:-shift, :] = 0.
+                                    sam_shifted = np.roll(sam, shift, axis=0 ) 
                                     if not mix.shape==sam.shape:
-                                        mix = sam*weight
-                                        pt_edges = sample[2]
-                                        y_edges = sample[1]
+                                        mix = sam_shifted*weight
                                     else:
-                                        mix += sam*weight
+                                        mix += sam_shifted*weight
 
         #print(mix)
+        out_name = 'pt{:02.1f}'.format(pt_bin[0])+'-'+'{:02.1f}'.format(pt_bin[1])+'_'+'y{:02.1f}'.format(y_bin[0])+'-'+'y{:02.1f}'.format(y_bin[1])+'_M{:05.3f}'.format(mass)
+        for c in range(5):
+            out_name += ('_A'+str(c)+('{:03.2f}'.format(coeff[c])))
+        np.save(self.output_dir+'/mixed_dataset_'+out_name, mix)
 
         xx, yy = np.meshgrid(pt_edges, y_edges)        
         plt.pcolormesh(yy, xx, mix)
         plt.show()
-        plt.savefig(self.output_dir+'/mixed_dataset.png')
-
+        plt.savefig(self.output_dir+'/mixed_dataset_'+out_name+'.png')
 
 ##################################
--- python/template_parameters.py
+++ python/template_parameters.py
@@ -19,6 +19,17 @@ Parameters = {
     'params_W' : {
         'pt' : np.linspace(0.0, 20., 2.0),
         'y'  : np.linspace(-3.0, 3.0, 0.5),
+        'mass' : np.linspace(78.000, 82.000, 2),
+        'A0' : np.array([0.0]),
+        'A1' : np.array([0.0]),
+        'A2' : np.array([0.0]),
+        'A3' : np.array([0.0]),
+        'A4' : np.array([0.0]),
+        },    
+
+    'params_template' : {
+        'pt' : np.array([0.0, 2.0, 5.0, 10., 20.,]),
+        'y'  : np.linspace(-3.0, 3.0, 31),
         'mass' : np.linspace(78.000, 82.000, 0.500),
         'A0' : np.array([0.0]),
         'A1' : np.array([0.0]),
@@ -29,32 +40,13 @@ Parameters = {
 }
 
 def pdf_test(pt,y, c0, c1, c2, c3, c4):
+
     val = 1.0
-    return val
 
     # pt
-    if pt >= 0. and pt < 2.0:
-        val *= 0.025
-    elif pt >= 2.0 and pt < 4.0:
-        val *= 0.050
-    elif pt >= 4.0 and pt < 6.0:
-        val *= 0.055
-    elif pt >= 6.0 and pt < 8.0:
-        val *= 0.05
-    elif pt >= 8.0 and pt < 10.0:
-        val *= 0.04
-    elif pt >= 10.0 and pt < 12.0:
-        val *= 0.035
-    elif pt >= 12.0 and pt < 14.0:
-        val *= 0.03
-    elif pt >= 14.0 and pt < 16.0:
-        val *= 0.025
-    elif pt >= 14.0 and pt < 18.0:
-        val *= 0.02
-    elif pt >= 18.0 and pt < 20.0:
-        val *= 0.015
-    else:
-        val *= 0.01
+    pt_max = 5.0
+    lambdaQCD = 0.200
+    val *= math.exp(-(pt+lambdaQCD)/pt_max)*(pt+lambdaQCD)/pt_max/pt_max
 
     # y        
     y_width = 1.5
@@ -62,16 +54,25 @@ def pdf_test(pt,y, c0, c1, c2, c3, c4):
     
     # coeff
     #val *= 1.0 if c4==-y/2.5 else 0.0
-    val *= (1.0 if c4*y <= 0. else 0.0)
+    #val *= (1.0 if c4*y <= 0. else 0.0)
     return val
 
 
 params_test = copy.deepcopy(Parameters)
-params_test['params_W']['pt'] = np.array([0.0]) 
-params_test['params_W']['y'] = np.linspace(-2.5,2.5,201) 
-params_test['params_W']['mass'] = np.array([80.000]) 
-#params_test['params_W']['A0'] = np.array([0.0]) 
-#params_test['params_W']['A1'] = np.array([0.0]) 
-#params_test['params_W']['A2'] = np.array([0.0])
-#params_test['params_W']['A3'] = np.array([0.0]) 
+params_test['params_W']['pt'] = np.linspace(0.0, 10., 11) 
+params_test['params_W']['y'] = np.array([0.0])
+params_test['params_W']['mass'] = np.array([80.000])
 params_test['params_W']['A0'] = np.array([0.0]) 
+params_test['params_W']['A1'] = np.array([0.0]) 
+params_test['params_W']['A2'] = np.array([0.0])
+params_test['params_W']['A3'] = np.array([0.0]) 
+params_test['params_W']['A4'] = np.array([0.0])
+
+params_test['params_template']['pt'] = np.array([0.0, 2.0, 4.0, 6.0, 8.0, 10.0])
+params_test['params_template']['y']  = np.array([-0.2, 0.0,  0.2])
+params_test['params_template']['mass'] = np.array([80.000])#params_test['params_W']['mass']
+params_test['params_template']['A0'] = params_test['params_W']['A0']
+params_test['params_template']['A1'] = params_test['params_W']['A1']
+params_test['params_template']['A2'] = params_test['params_W']['A2']
+params_test['params_template']['A3'] = params_test['params_W']['A3']
+params_test['params_template']['A4'] = params_test['params_W']['A4']
