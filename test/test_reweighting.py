1af314a1f09eb7fe03bc59427c7eb2233608570a
bianchini <lore.bianchini@gmail.com>
2018-04-16 12:47:16 +0200

    boh

--- test/test_reweighting.py
+++ test/test_reweighting.py
@@ -3,33 +3,39 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-ul = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A10.00_A20.00_A30.00_A40.00.npy')
+dirname = 'TEST'
+
+ul = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A20.00_A30.00_A40.00.npy')
 p1 = np.array([])
 p2 = np.array([])
 
-A = 1.0
+A = 2.0
+if True:    
+    p1 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A02.00_A10.00_A20.00_A30.00_A40.00.npy')
+    p2 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A0'+'{:03.2f}'.format(A)+'_A10.00_A20.00_A30.00_A40.00.npy')
+    #A = 0.5
 if False:
-    p1 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A02.00_A10.00_A20.00_A30.00_A40.00.npy')
-    p2 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A0'+'{:03.2f}'.format(A)+'_A10.00_A20.00_A30.00_A40.00.npy')
+    p1 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A11.00_A20.00_A30.00_A40.00.npy')
+    p2 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A1'+'{:03.2f}'.format(A)+'_A20.00_A30.00_A40.00.npy')
+if False:
+    p1 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A21.00_A30.00_A40.00.npy')
+    p2 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A2'+'{:03.2f}'.format(A)+'_A30.00_A40.00.npy')
+if False:
+    p1 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A20.00_A31.00_A40.00.npy')
+    p2 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A20.00_A3'+'{:03.2f}'.format(A)+'_A40.00.npy')
+if False:
+    p1 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A20.00_A30.00_A42.00.npy')
+    p2 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A20.00_A30.00_A4'+'{:03.2f}'.format(A)+'.npy')
     A = 0.5
 if False:
-    p1 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A11.00_A20.00_A30.00_A40.00.npy')
-    p2 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A1'+'{:03.2f}'.format(A)+'_A20.00_A30.00_A40.00.npy')
-if False:
-    p1 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A10.00_A21.00_A30.00_A40.00.npy')
-    p2 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A10.00_A2'+'{:03.2f}'.format(A)+'_A30.00_A40.00.npy')
-if False:
-    p1 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A10.00_A20.00_A31.00_A40.00.npy')
-    p2 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A10.00_A20.00_A3'+'{:03.2f}'.format(A)+'_A40.00.npy')
-if True:
-    p1 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A10.00_A20.00_A30.00_A42.00.npy')
-    p2 = np.load('../root//grid_lab_pt6.0_y0.00_M80.000_A00.00_A10.00_A20.00_A30.00_A4'+'{:03.2f}'.format(A)+'.npy')
-    A = 0.5
+    p1 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.000_A00.00_A10.00_A20.00_A30.00_A40.00.npy')
+    p2 = np.load('../root/'+dirname+'/grid_lab_pt5.0_y0.00_M80.500_A00.00_A10.00_A20.00_A30.00_A40.00.npy')
+    A = 0.02
 
 p2a = np.zeros(ul[0].shape) 
 p2b = np.zeros(ul[0].shape) 
-p2a += (ul[0]*(1-A) + p1[0]*A)
-p2b += (-ul[0] + p1[0])*2.0
+p2a += (ul[0]*(1-1./A) + p1[0]*1./A)
+p2b += (-ul[0] + p1[0])*(1./A)
 
 print p2a.sum(), '<=>' , p2[0].sum()
 
@@ -47,6 +53,7 @@ plt.colorbar()
 #plt.savefig('plain.png')
 
 diff = p2a-p2[0]
+#diff = A*(p2[0]-ul[0])
 plt.subplot(2, 2, 3)
 plt.pcolormesh(yy, xx, diff)
 plt.colorbar()
