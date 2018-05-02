c0108874f805745abeab0227722c0e7789b58e34
bianchini <lore.bianchini@gmail.com>
2018-05-02 17:05:19 +0200

    postjob for hadd

---
+++ test/postjob.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+hadd -f ../root/tree_histos1_CC_FxFx.root /gpfs/ddn/srm/cms/store/user/bianchi/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/TEST/180430_102338/0000/tree*root /gpfs/ddn/srm/cms/store/user/bianchi/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/TEST/180430_102433/0000/tree*root
+
+hadd -f ../root/tree_histos1_CC_MG5.root /gpfs/ddn/srm/cms/store/user/bianchi/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/TEST/180430_144642/0000/tree*root /gpfs/ddn/srm/cms/store/user/bianchi/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/TEST/180430_144741/0000/tree*root
