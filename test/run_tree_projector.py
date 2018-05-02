900c62daeb561cdeeeb788f2c7077c7fa8a7269d
bianchini <lore.bianchini@gmail.com>
2018-05-02 17:04:58 +0200

    compare

--- test/run_tree_projector.py
+++ test/run_tree_projector.py
@@ -28,44 +28,77 @@ elif argv[1]=='pt_bias':
 
 elif argv[1]=='fit':
     for coeff in ['A0','A1','A2','A3','A4','A5', 'A6', 'A7']:
-    #for coeff in ['A3']:
         draw_y_slice(fname='../root/tree_histos_CC.root', var='Wdress', coeff=coeff, weight_name=0, do_fit=True)
         #draw_qt_slice(fname='../root/tree_histos_NC.root', var='Wdress', coeff=coeff, weight_name=0, do_fit=False)
 
-elif 'cov' in argv[1]:
+elif argv[1]=='cov-all':
     weights = {}
     weights['scale'] = ([0] + [1,2,3,4,6,8])
     weights['pdf'] = range(9,109)
-    for q in [#'Wplus', 
-              'Wminus'
-              ]:
-        if 'all' in argv[1]:
-            get_covariance(fname='../root/tree_histos_CC.root', DY='CC', var='Wdress', q=q, weights=weights, 
-                           coefficients=['A0','A1','A2','A3','A4','A5','A6','A7'], 
-                           add_stat_uncert=True, postfix='all_A0-7',
-                           save_corr=True, save_coeff=True, save_tree=True, save_pkl=True)
-        else:
-            for coeff in ['A7']:
-                get_covariance(fname='../root/tree_histos_CC.root', DY='CC', var='Wdress', q=q, weights=weights, 
-                               coefficients=[coeff], 
-                               add_stat_uncert=True, postfix=coeff,
+    for DY in ['CC_FxFx', 'CC_MG5']:
+        for q in ['Wplus', 'Wminus']:
+            for var in ['Wdress', 'Wbare', 'WpreFSR']:
+                get_covariance(fname='../root/tree_histos1_'+DY+'.root', DY=DY, var=var, q=q, weights=weights, 
+                               coefficients=['A0','A1','A2','A3','A4','A5','A6','A7'], 
+                               add_stat_uncert=True, postfix='all_A0-7',
                                save_corr=True, save_coeff=True, save_tree=True, save_pkl=True)
+            
+elif argv[1]=='cov':
+    weights = {}
+    weights['scale'] = ([0] + [1,2,3,4,6,8])
+    weights['pdf'] = range(9,109)
+    for DY in ['CC_FxFx', 'CC_MG5']:
+        for q in ['Wplus']:
+            for var in ['Wdress', 'Wbare', 'WpreFSR']:
+                for coeff in ['A0']: #,'A1','A2','A3','A4','A5','A6','A7']: 
+                    get_covariance(fname='../root/tree_histos1_'+DY+'.root', DY=DY, var=var, q=q, weights=weights, 
+                                   coefficients=[coeff], 
+                                   add_stat_uncert=True, postfix=coeff,
+                                   save_corr=True, save_coeff=True, save_tree=True, save_pkl=True)
 
 elif argv[1]=='closure':
-    for q in ['Wminus']:
-        for ceval in ['val']:
-            # MC
-            plot_closure_test(charge=q,  var='Wdress', coeff_eval=ceval, coeff=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
-                              byInvPdf=False,
-                              save_2D=True, save_pdf=True, save_summary=True, 
-                              do_toy=False, extra_variance_toy=0.0)
-            # toy
-            plot_closure_test(charge=q,  var='Wdress', coeff_eval=ceval, coeff=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
-                              byInvPdf=False,
-                              save_2D=False, save_pdf=False, save_summary=True, 
-                              do_toy=True, extra_variance_toy=0.001)
-            # MC byInvPdf
-            plot_closure_test(charge=q,  var='Wdress', coeff_eval=ceval, coeff=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
-                              byInvPdf=True,
-                              save_2D=True, save_pdf=False, save_summary=True, 
-                              do_toy=False, extra_variance_toy=0.0)            
+    for DY in ['CC_FxFx', 'CC_MG5']:
+        for q in ['Wplus', 'Wminus']:
+            for var in ['Wdress', 'Wbare', 'WpreFSR']:
+                for ceval in ['val']:
+                    # MC
+                    plot_closure_test(charge=q,  DY=DY, var=var, coeff_eval=ceval, coeff=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
+                                      byInvPdf=False,
+                                      save_2D=True, save_pdf=False, save_summary=False, 
+                                      do_toy=False, extra_variance_toy=0.0)
+                    # toy
+                    plot_closure_test(charge=q,  DY=DY, var=var, coeff_eval=ceval, coeff=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
+                                      byInvPdf=False,
+                                      save_2D=False, save_pdf=False, save_summary=True, 
+                                      do_toy=True, extra_variance_toy=0.001)
+                    # MC byInvPdf
+                    plot_closure_test(charge=q,  DY=DY, var=var, coeff_eval=ceval, coeff=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
+                                      byInvPdf=True,
+                                      save_2D=True, save_pdf=False, save_summary=True, 
+                                      do_toy=False, extra_variance_toy=0.0)            
+
+elif argv[1]=='compare':
+    compare_fit_results(DYs=['CC_FxFx'], 
+                        charges=['Wplus'], 
+                        variables=['Wbare', 'Wdress', 'WpreFSR'], 
+                        coefficients=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
+                        fit_range=[0.0, 50.0], postfix='all_A0-7', 
+                        compare='CC_FxFx_Wplus_dressing')
+    compare_fit_results(DYs=['CC_FxFx'], 
+                        charges=['Wminus'], 
+                        variables=['Wbare', 'Wdress', 'WpreFSR'], 
+                        coefficients=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
+                        fit_range=[0.0, 50.0], postfix='all_A0-7', 
+                        compare='CC_FxFx_Wminus_dressing')
+    compare_fit_results(DYs=['CC_FxFx', 'CC_MG5'], 
+                        charges=['Wplus'], 
+                        variables=['Wdress'], 
+                        coefficients=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
+                        fit_range=[0.0, 50.0], postfix='all_A0-7', 
+                        compare='CC_Wplus_Wdress_generator')
+    compare_fit_results(DYs=['CC_FxFx', 'CC_MG5'], 
+                        charges=['Wminus'], 
+                        variables=['Wdress'], 
+                        coefficients=['A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'], 
+                        fit_range=[0.0, 50.0], postfix='all_A0-7', 
+                        compare='CC_Wminus_Wdress_generator')
